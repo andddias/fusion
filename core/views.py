74d274924c49152766cfb8a916e55d47135c51bd
Andre Dias <and.ddias@gmail.com>
2020-02-23 18:34:02 -0300

    Conhecendo e Utilizando o Context Manager

--- core/views.py
+++ core/views.py
@@ -1,5 +1,14 @@
 from django.views.generic import TemplateView
 
+from .models import Servico, Funcionario
+
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        # obtendo os servicos de forma aleatoria pelo order_by('?')
+        context['servicos'] = Servico.objects.order_by('?').all()
+        context['funcionarios'] = Funcionario.objects.order_by('?').all()
+        return context
