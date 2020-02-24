5b66adb3f4f40d6e52a8a9165f6d42001acceb2f
Andre Dias <and.ddias@gmail.com>
2020-02-24 19:37:16 -0300

    Atualização modo Administrativo Django

--- fusion/settings.py
+++ fusion/settings.py
@@ -32,14 +32,15 @@ ALLOWED_HOSTS = ['fusion-and.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'core',
+    'django_adminlte',
+    'django_adminlte_theme',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'core',
 ]
 
 MIDDLEWARE = [
